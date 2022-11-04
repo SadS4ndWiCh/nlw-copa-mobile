45794403a05781c16ccdc81c06868c49676e7d1a
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-11-04 16:56:33 -0300

    wrap children element with auth provider

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import { NativeBaseProvider } from "native-base";
 
 import { FontsProvider } from "./FontsProvider";
+import { AuthContextProvider } from "./AuthProvider";
 
 import { THEME } from "../styles/theme";
 
@@ -13,7 +14,9 @@ export const AppProviders = ({ children }: Props) => {
 	return (
 		<NativeBaseProvider theme={THEME}>
 			<FontsProvider>
-				{ children }
+				<AuthContextProvider>
+					{ children }
+				</AuthContextProvider>
 			</FontsProvider>
 		</NativeBaseProvider>
 	)
